fe2d59e0fd7d6a4889a726a5969b2a58b193201e
Eka Fajhari Adwar <eka.fajhari@qonex.id>
2025-03-04 16:53:43 +0700

    Membuat pagination di halaman Berries

--- src/views/BerriesView.vue
+++ src/views/BerriesView.vue
@@ -6,50 +6,65 @@
       <div class="w-full sm:w-lg">
         <div class="flex flex-col sm:flex-row space-y-2 justify-between my-5">
           <input class="bg-white px-4 py-2 rounded-sm" type="text" v-model="searchQuery" :placeholder="`${ $t('search') }`" />
-          <button class="bg-emerald-300 px-4 py-2 rounded-sm">
-              {{$t("sort")}}
-              <font-awesome-icon :icon="['fas', 'arrow-down-a-z']" />
-              <!-- <font-awesome-icon v-else :icon="['fas', 'arrow-up-a-z']" /> -->
-            </button>
         </div>
 
         <table class="w-full">
-        <thead>
-          <tr>
-            <th class="text-left">No</th>
-            <th class="text-left">Nama</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(berry, index) in filteredBerries" :key="berry.name" >
-            <td class="py-2 border-t-1 border-gray-200">{{ index + 1 }}</td>
-            <td class="py-2 border-t-1 border-gray-200">{{ berry.name }}</td>
-            <td class="py-2 border-t-1 border-gray-200"><button class="bg-gray-800 text-white px-4 py-2 rounded-sm" @click="showDetail(berry)"><font-awesome-icon :icon="['fas', 'eye']" /></button></td>
-          </tr>
-        </tbody>
-      </table>
+          <thead>
+            <tr>
+              <th class="text-left">No</th>
+              <th class="text-left">Nama</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(berry, index) in berries" :key="berry.name">
+              <td class="py-2 border-t-1 border-gray-200">{{ (currentPage - 1) * itemsPerPage + index + 1 }}</td>
+              <td class="py-2 border-t-1 border-gray-200">{{ berry.name }}</td>
+              <td class="py-2 border-t-1 border-gray-200"><button class="bg-gray-800 text-white px-4 py-2 rounded-sm" @click="showDetail(berry)"><font-awesome-icon :icon="['fas', 'eye']" /></button></td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div class="flex justify-between items-center mt-4">
+          <button @click="prevPage" :disabled="currentPage === 1" class="bg-gray-800 text-white px-4 py-2 rounded-sm">
+            Previous
+          </button>
+          <span>Page {{ currentPage }} of {{ totalPages }}</span>
+          <button @click="nextPage" :disabled="currentPage === totalPages" class="bg-gray-800 text-white px-4 py-2 rounded-sm">
+            Next
+          </button>
+        </div>
+
+        <div class="mt-4">
+          <label for="itemsPerPage">Show:</label>
+          <select id="itemsPerPage" v-model="itemsPerPage" class="ml-2 p-2 border rounded">
+            <option :value="10">10</option>
+            <option :value="30">30</option>
+            <option :value="50">50</option>
+          </select>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from "vue";
-import axios from "axios";
+import { onMounted } from "vue";
+import { useBerryStore } from "@/stores/berryStore";
+import { storeToRefs } from "pinia";
 
-const berries = ref([]);
-const searchQuery = ref("");
+const {
+  fetchBerries,
+  prevPage,
+  nextPage,
+} = useBerryStore();
 
-const fetchBerries = async () => {
-  const res = await axios.get("https://pokeapi.co/api/v2/berry");
-  berries.value = res.data.results;
-};
-
-const filteredBerries = computed(() =>
-  berries.value.filter((b) =>
-    b.name.toLowerCase().includes(searchQuery.value.toLowerCase())
-  )
-);
+const {
+  berries,
+  searchQuery,
+  currentPage,
+  itemsPerPage,
+  totalPages,
+} = storeToRefs(useBerryStore())
 
 const showDetail = (berry) => {
   alert("Berry: " + berry.name);
